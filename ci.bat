46d5b67e61505774707705b8f600ec0a1799599c
xfade2111-dotcom <xfade2111@gmail.com>
2026-04-05 20:41:37 +0300

    Update ci.bat with build configuration instructions

--- ci.bat
+++ ci.bat
@@ -1,1 +1,18 @@
+REM Створення каталогу білдування
+mkdir build
 
+REM Перехід у каталог білдування
+cd build
+
+REM Конфігурування проєкту за допомогою CMake
+cmake ..
+
+REM Білдування проєкту
+cmake --build .
+
+REM Повернення у кореневий каталог проєкту
+cd ..
+
+REM Забезпечення прав на виконання та фіксація змін (використовуємо git)
+git add -A
+git commit -m "Build configuration and executable permissions"
